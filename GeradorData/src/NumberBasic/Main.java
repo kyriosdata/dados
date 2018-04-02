37777c976bee75d11a5273f430453f55c832b900
Andoly <andoly.cs@hotmail.com>
2018-04-02 00:55:46 -0300

    Revert "Revert "kyriosdata/dados""
    This reverts commit 1fd337bfc4f5e52c37fd40e1223cfdbd05a6acc9.

---
+++ GeradorData/src/NumberBasic/Main.java
@@ -0,0 +1,19 @@
+
+package NumberBasic;
+
+/**
+ 
+ * @author Andoly
+ */
+public class Main {
+    
+        public static void main(String[] args) {
+        
+        Num NM = new Num();
+        NM.Num();
+            
+      
+        }
+            
+}
+
