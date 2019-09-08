16ffb5183161289cfb7e611d8cb79b2c72fd2a8c
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2019-09-08 16:07:58 -0300

    clean

--- gerador/src/test/java/com/github/kyriosdata/dados/GeradorTest.java
+++ gerador/src/test/java/com/github/kyriosdata/dados/GeradorTest.java
@@ -11,10 +11,29 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GeradorTest {
+
+    @Test
+    void redundancias() {
+        List<String> cartorios = new Gerador().cartorios();
+        Set<String> nomes =
+                cartorios.stream().map(c -> c.split(";")[0]).collect(Collectors.toSet());
+        Set<String> fantasias =
+                cartorios.stream().map(c -> c.split(";"))
+                        .filter(e -> e.length > 1)
+                        .map(a -> a[1])
+                        .collect(Collectors.toSet());
+        System.out.println(cartorios.size());
+        System.out.println(nomes.size());
+        System.out.println(fantasias.size());
+        nomes.forEach(System.out::println);
+        fantasias.forEach(System.out::println);
+    }
 
     @Test
     void recuperaNomesSobrenomes() {
